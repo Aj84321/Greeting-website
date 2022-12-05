0d89ff5d40244d01cc75fb812d68c8c99722f98a
Anand Jain <jainanand84321@gmail.com>
2022-12-05 16:43:15 +0530

    greeting website

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDom from "react-dom";
+import './index.css';
+
+let curDate = new Date (2022,  8, 3, 12);
+curDate = curDate.getHours();
+let greeting = " ";
+const cssStyle = {};
+
+if (curDate >= 1 && curDate < 12){
+
+   greeting = "Good Morning";
+   cssStyle.color = "green";
+   }
+else if (curDate >= 12 && curDate <19){
+greeting = "Good Afternoon";
+cssStyle.color = "black";
+}
+else {greeting = "Good Night";
+cssStyle.color = "pink";
+}
+ReactDom.render(
+   <> 
+   <div>
+   <h1>
+    Hello Sir,  <span style ={cssStyle}> {greeting} </span>
+   </h1> 
+   </div>
+   </>,
+   document.getElementById("root")
+   );
